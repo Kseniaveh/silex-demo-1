f3861741f2caf9098f8f251bee1d9d12a4a24f44
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-06-22 22:52:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,22 @@
     return '<h2>Сумма: </h2><h3>' . ($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
 
+  $app->get('/sub/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Вычитание: </h2><h3>' . ($n1 - $n2) . '</h3>'; 
+  })->after($hdrs);
+
+  $app->get('/div/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Деление: </h2><h3>' . ($n1 / $n2) . '</h3>'; 
+  })->after($hdrs);
+
+  $app->get('/mul/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Умножение: </h2><h3>' . ($n1 * $n2) . '</h3>'; 
+  })->after($hdrs);
+
+  $app->get('/pow/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Возведение в степень: </h2><h3>' . ($n1 ** $n2) . '</h3>'; 
+  })->after($hdrs);
+
   $app->error(function ($e) use($app) {
     if ($e instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
       return new Response($app['twig']->render('404.twig'), 404);
