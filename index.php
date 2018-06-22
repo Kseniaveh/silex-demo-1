a8076a0f8555d81645c61a3926cab45255eabd95
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-06-22 23:20:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,10 @@
     return '<h2>Возведение в степень: </h2><h3>' . ($n1 ** $n2) . '</h3>'; 
   })->after($hdrs);
 
+   $app->get('/author/', function use($app) {
+    return '<h2>Вехова Ксения</h2>'; 
+  })->after($hdrs);
+
   $app->error(function ($e) use($app) {
     if ($e instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
       return new Response($app['twig']->render('404.twig'), 404);
